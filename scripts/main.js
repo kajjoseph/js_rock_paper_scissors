c1a799b4e7ad85e00fa17a6e3470de3f3af2c37f
Kaj Joseph <kajjoseph@gmail.com>
2020-09-28 13:40:50 -0600

    Added game logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,45 @@
-function computerPlay() {
-    let choice = Math.floor(Math.random() * 3);
-    switch (choice) {
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors';
+let buttons = document.querySelectorAll('.play-buttons');
+buttons.forEach(button => button.addEventListener('click', playRound));
+let output = document.querySelector('.output')
+function comparePlays (cpu, human) {
+    if (cpu == human) {
+        return 'tie';
+    } else if (human == 'rock') {
+        if (cpu == 'paper') {
+            return 'cpu';
+        } else if (cpu == 'scissors') {
+            return 'human';
+        }
+    } else if (human == 'paper') {
+        if (cpu == 'rock') {
+            return 'human';
+        } else if (cpu == 'scissors') {
+            return 'cpu';
+        }
+    } else if (human == 'scissors') {
+        if (cpu == 'rock') {
+            return 'cpu';
+        } else if (cpu == 'paper') {
+            return 'human';
+        }
     }
+
 }
-function compareChoices (playerChoice, computerChoice) {
-    // TODO: Devise programmatic method of camparing choices;
-    // TODO: Add more robust error checking; currently invalid input corrupts future data;
-    if (playerChoice == 'rock') {
-        switch (computerChoice) {
-            case 'rock':
-                return 'tie';
-            case 'paper':
-                return 'cpu';
-            case 'scissors':
-                return 'player';
-        }
-    } else if (playerChoice == 'paper') {
-        switch (computerChoice) {
-            case 'rock':
-                return 'player';
-            case 'paper':
-                return 'tie';
-            case 'scissors':
-                return 'cpu';
-        }
-    } else if (playerChoice == 'scissors') {
-        switch (computerChoice) {
-            case 'rock':
-                return 'cpu';
-            case 'paper':
-                return 'player';
-            case 'scissors':
-                return 'tie';
-        }
-    }
+function playRound (e) {
+    let playerChoice = e.target.id;
+    let choices = {0: 'rock', 1: 'paper', 2: 'scissors'};
+    let computerChoice = choices[Math.floor(Math.random()*3)];
+    let para = document.createElement('p');
+    let newBreak = document.createElement('br');
+    let cpuPlay = document.createTextNode(`CPU: ${computerChoice}`);
+    let humanPlay = document.createTextNode(`Player: ${playerChoice}`);
+    let decision = comparePlays(computerChoice, playerChoice);
+    let decisionText = document.createTextNode(`Winner: ${decision}`)
+    para.appendChild(cpuPlay);
+    para.appendChild(newBreak);
+    para.appendChild(humanPlay);
+    para.appendChild(document.createElement('br'));
+    para.appendChild(decisionText);
+    output.appendChild(para);
+
 }
